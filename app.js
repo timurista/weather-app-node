bb0f2beb72120cd84b50c59cb1503b7c79cd2c9c
Tim Urista <timothy.urista@gmail.com>
2017-10-13 16:55:09 -0700

    master update app.js in app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const request = require('request');
+const _ = require('lodash');
 
 request({
   url: "http://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lambard%20Street%20Philadelphia",
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  // pretty print
+  // use json stringify, then add undefined to not replace, and value of 2 to format
+  console.log(JSON.stringify(body, undefined, 2));
+
+  const address = _.get(body, 'results[0].formatted_address');
+  const { lat, lng } = _.get(body, 'results[0].geometry.location');
+  
+  console.log(`Address: ${address}`);
+  console.log(`LAT: ${lat}, LNG: ${lng}`);
 });
