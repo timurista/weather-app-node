98dc285ddf537d9ed9682740173680ee78b88c83
Tim Urista <timothy.urista@gmail.com>
2017-10-14 14:28:07 -0700

    master update .gitignore in .gitignore

--- app-promise.js
+++ app-promise.js
@@ -1,40 +1,63 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const fs = require('fs');
 const { weather_key } = require('./SECRET_KEYS');
 
 const argv = yargs
   .options({
     a: {
-      demand: true,
+      demand: false,
       alias: 'address',
       describe: 'Address to fetch weather for',
       string: true
+    },
+    d: {
+      alias: 'default',
+      describe: 'Flag to save the address as default'
     }
   })
   .help()
   .alias('help', 'h')
   .argv;
 
-const encodedAddress = encodeURIComponent(argv.address);
-const geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+function main() {
+  const defaultJsonPath = 'defaultAddress.json';
+  const defaultExists = fs.existsSync(defaultJsonPath);
+  let address = argv.address;
+  const loadDefault = !address && defaultExists;  
 
-axios.get(geocodeUrl)
-  .then(response => {
-    if (response.data.status === 'ZERO_RESULTS') throw new Error('Unable to find that address.');
-    const { lat, lng } = response.data.results[0].geometry.location;
-    console.log(response.data.results[0].formatted_address);  
-    const weatherUrl = `https://api.darksky.net/forecast/${weather_key}/${lat},${lng}`;    
-    return axios.get(weatherUrl);
-  })
-  .then(response => {
-    const { temperature, apparentTemperature, summary } = response.data.currently;
-    // console.log(JSON.stringify(response.data.currently, undefined, 2))
-    console.log(`Weather is: ${summary}. It is ${temperature} and feels like ${apparentTemperature}`);
-  })
-  .catch(e => {
-    if (e.code=== 'ENOTFOUND') console.log('Unable to connect to server');
-    console.log(e.message);
-  });
-// include more information
+  if (loadDefault) {
+    address = JSON.parse(fs.readFileSync(defaultJsonPath)).default;
+    console.log('using default address...');
+  } else if (!address) {
+    console.log('No address passed in and no default address exists');
+    return false;
+  }
+  
+  if (argv.default && address) {
+    console.log(`Default address saved: ${address}`)    
+    fs.writeFileSync(defaultJsonPath, JSON.stringify({default: address}));
+  }
+  const encodedAddress = encodeURIComponent(address);
+  const geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+  
+  axios.get(geocodeUrl)
+    .then(response => {
+      if (response.data.status === 'ZERO_RESULTS') throw new Error('Unable to find that address.');
+      const { lat, lng } = response.data.results[0].geometry.location;
+      console.log(response.data.results[0].formatted_address);
+      const weatherUrl = `https://api.darksky.net/forecast/${weather_key}/${lat},${lng}`;    
+      return axios.get(weatherUrl);
+    })
+    .then(response => {
+      const { temperature, apparentTemperature, summary } = response.data.currently;
+      // console.log(JSON.stringify(response.data.currently, undefined, 2))
+      console.log(`Weather is: ${summary}. It is ${temperature} and feels like ${apparentTemperature}`);
+    })
+    .catch(e => {
+      if (e.code=== 'ENOTFOUND') console.log('Unable to connect to server');
+      console.log(e.message);
+    });
+}
 
-// default location
+main();
